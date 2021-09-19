ea9e109e333445f3418f370c420267535a1c1e2a
unknown <k_m_naoto@yahoo.co.jp>
2021-09-19 17:07:20 +0900

    追加：注釈周りのあれこれ

--- html-page.ts
+++ html-page.ts
@@ -54,7 +54,40 @@ export function getHtmlPage(content: string, side_content: string) {
   .sidebar-fixed {
     position: sticky;
     top: 100px;
-    font-size: 75%;
+    font-size: 80%;
+  }
+
+  /* カーソルを重ねる要素 */
+  .tooltip {
+    position: relative; /* ツールチップの位置の基準に */
+    cursor: pointer; /* カーソルを当てたときにポインターに */
+    padding-top: 120px;
+    margin-top: -120px;
+  }
+
+  /* ツールチップのテキスト */
+  .tooltip-text {
+    opacity: 0; /* はじめは隠しておく */
+    visibility: hidden; /* はじめは隠しておく */
+    position: absolute; /* 絶対配置 */
+    left: 50%; /* 親に対して中央配置 */
+    transform: translateX(-50%); /* 親に対して中央配置 */
+    top: 150px; 
+    display: inline-block;
+    padding: 5px; /* 余白 */
+    white-space: nowrap; /* テキストを折り返さない */
+    font-size: 0.8rem; /* フォントサイズ */
+    line-height: 1.3; /* 行間 */
+    background: #333; /* 背景色 */
+    color: #fff; /* 文字色 */
+    border-radius: 3px; /* 角丸 */
+    transition: 0.3s ease-in; /* アニメーション */
+  }
+
+  /* ホバー時にツールチップの非表示を解除 */
+  .tooltip:hover .tooltip-text {
+    opacity: 1;
+    visibility: visible;
   }
 
   </style>
--- server.ts
+++ server.ts
@@ -5,9 +5,19 @@ import { App, Controller, Get, View } from "https://deno.land/x/alosaur_lite/dis
 import { getHtmlPage } from "./html-page.ts";
 
 
-const ADD_LINE_NUM = true
-let HEADER_STORE: string[] = []
+interface FtnoteInfo {
+  index : number;
+  text : string;
+  is_tip_set : boolean;
+}
 
+
+const SCRAP_SEPARATION = "END_OF_SCRAP";
+const ADD_LINE_NUM = true;
+let INPAGE_FT_COUNT = 0;
+let HEADER_STORE: string[] = [];
+let FOOTNOTE_STORE = new Map<number, FtnoteInfo>();
+let TOOLTIP_STORE= new Map<string, string>();
 
 // -----------------------------
 
@@ -47,6 +57,72 @@ function addLineNumbers (inputHtml: string, use_singleLine=false, startFrom=1): 
 }
 
 
+// html タグを消去しつつ文章を固定文字数に切り分ける関数
+function chunk_text(text: string, num=33): string[]{
+  text = text.replace(/&quot;/g, '"').replace(/<\/??\w.+?>/g, " ")
+  let L: string[] = [];
+
+  let count = 1;
+  if (count*num > text.length) {
+    return [text]
+  }
+  while (count*num <= text.length) {
+    L.push(text.slice((count-1)*num, count*num));
+    count++
+  }
+  L.push(text.slice((count-1)*num));
+  return L
+}
+
+
+function get_refnumber_html (ft_count: number, content: string): string{
+  let style_setting = ""
+  if (content.length > 30) {
+    style_setting = ' style="min_width=450px; white-space: break-spaces;"'
+  }
+  const ref_text = `
+    <sup class="tooltip" id="fn_ref:${ft_count}">
+      <a href="#fn:${ft_count}">(${ft_count})</a>
+      <span class="tooltip-text"${style_setting}>${content}</span>
+    </sup>`;
+  return ref_text
+}
+
+
+function make_footnote(): string{
+  const keys = [...FOOTNOTE_STORE.keys()].sort()
+  const start_num = keys[0];
+  const L = keys.map((idx:number) =>{
+    const ft_info = FOOTNOTE_STORE.get(idx)
+    if (ft_info !== undefined){
+      const ft_text = `
+        <li id="fn:${ft_info.index}" style="padding-top:120px; margin-top:-120px;">
+          <font size="1">${ft_info.text}</font>
+          <a href="#fn_ref:${ft_info.index}">  ↩</a>
+        </li>`;
+      if (!ft_info.is_tip_set) {
+        TOOLTIP_STORE.set(`TEMP_TOOLTIP_${idx}`,ft_info.text);
+      }
+      return ft_text;
+    } else {
+      return ""
+    }
+  })
+  INPAGE_FT_COUNT = INPAGE_FT_COUNT + Math.max(...keys);
+  FOOTNOTE_STORE = new Map<number, FtnoteInfo>();
+  if (L.length) {
+    const ft_htmls = `
+      <div style="border-bottom:1px solid #b9b9b9;font-size:90%">脚注</div>
+      <ol start="${start_num}" style="color:dimgray;">
+        ${L.join("\n")}
+      </ol><br>
+      `;
+    return ft_htmls;
+  } else {
+    return ""
+  }
+}
+
 
 // -----------------------------
 
@@ -85,6 +161,62 @@ class MyRenderer extends Renderer{
       //console.log(text);
     }
     return '<li>' + text + '</li>\n';
+  }
+
+  link(href: string, title: string, text: string): string {
+    if (text.match(/\^(\d+)/) !== null) {
+      let footnote_count = Number(text.match(/\^(\d+)/)![1]) + INPAGE_FT_COUNT
+      /*if (FOOTNOTE_STORE.has(footnote_count)) {
+        footnote_count = Math.max(...FOOTNOTE_STORE.keys()) +1
+      }*/
+      const ft_info: FtnoteInfo = {
+        index: footnote_count,
+        text: href,
+        is_tip_set: true
+      }
+      FOOTNOTE_STORE.set(footnote_count, ft_info)
+      return get_refnumber_html(footnote_count, href)
+    }
+    return super.link(href, title, text)
+  }
+
+  paragraph(text: string): string {
+    // 注釈内容の html の追加
+    if (text.includes(SCRAP_SEPARATION)) {
+      let ft_html = ""
+      if ([...FOOTNOTE_STORE.keys()].length) {
+        ft_html = make_footnote()
+      }
+      return `${ft_html}<hr><hr>\n`
+    }
+    
+    if ([...text.matchAll(/^\[\^(\d+)\]:/g)].length) {
+      const text_lis =  text.split(/\n/g);
+      text_lis.forEach((tx:string) => {
+        const [num, ref_text] = [...tx.matchAll(/^\[\^(\d+)\]: ([\s\S]+)/g)][0].slice(1);
+        const footnote_count = Number(num) + INPAGE_FT_COUNT;
+        if (FOOTNOTE_STORE.has(footnote_count)) {
+          FOOTNOTE_STORE.get(footnote_count)!.text = ref_text
+        }  
+      })
+      return ""
+    }
+
+    if ([...text.matchAll(/\[\^(\d+)\]/g)].length) {
+      [...text.matchAll(/\[\^(\d+)\]/g)].forEach( (matched: string[]) => {
+          const footnote_count = Number(matched[1]) + INPAGE_FT_COUNT;
+          const ft_info: FtnoteInfo = {
+            index: footnote_count,
+            text: `TEMP_TOOLTIP_${footnote_count}`,
+            is_tip_set: false
+          };
+          FOOTNOTE_STORE.set(footnote_count, ft_info);
+          const new_html = get_refnumber_html(footnote_count, `TEMP_TOOLTIP_${footnote_count}`);
+          text = text.replace(matched[0], new_html);
+      })
+      return `<p>${text}</p>\n`
+    }
+    return super.paragraph(text);
   }
 }
 
@@ -129,18 +261,29 @@ async function getMarkDownPage(path: string, config: any) {
   if (globalRenderCache.has(path)) {
     return globalRenderCache.get(path);
   }
-  let result = null;
+  let matchedt = null;
 
   const text = await Deno.readTextFile(`${Deno.cwd()}/views/${path}`);
-  const parsed = Marked.parse(text).content
-  const side_texts = HEADER_STORE.join("<br><br>")
-  HEADER_STORE = []
+  let parsed = Marked.parse(text).content;
+  if ([...TOOLTIP_STORE.keys()].length) {
+    TOOLTIP_STORE.forEach((tooltip_text: string, target_txt: string) => {
+      if (tooltip_text.length > 30) {
+        const syle_setting = ' style="min-width:450px; white-space: break-spaces;"';
+        parsed = parsed.replace(`>${target_txt}`, `${syle_setting}}>${tooltip_text}`);
+      } else {
+        parsed = parsed.replace(target_txt, tooltip_text);
+      }
+    })
+  }
+  TOOLTIP_STORE = new Map<string, string>()
+  const side_texts = HEADER_STORE.join("<br><br>");
+  HEADER_STORE = [];
 
-  result = getHtmlPage(parsed, side_texts);
+  matchedt = getHtmlPage(parsed, side_texts);
 
-  globalRenderCache.set(path, result);
+  globalRenderCache.set(path, matchedt);
 
-  return result;
+  return matchedt;
 }
 
 addEventListener("fetch", (event: FetchEvent) => {
